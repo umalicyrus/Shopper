ab49d2975f54ac8e73876b7543c2d9be5dbb0e5f
cyrusumali <umalicyrus04171998@gmail.com>
2024-08-15 10:28:11 +0800

    Added new collection and footer

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -4,6 +4,7 @@ import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
+import Footer from '../Components/Footer/Footer'
 
 export const Shop = () => {
   return (
@@ -12,7 +13,7 @@ export const Shop = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
-      {/* <NewsLetter/> */}
+      <NewsLetter/>
     </div>
   )
 }
