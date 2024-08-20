43da05ed24e681cd9102be0f8d58d500e5cd2f8f
cyrusumali <umalicyrus04171998@gmail.com>
2024-08-20 14:54:44 +0800

    Enhance responsiveness and design for shopcategory

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-cointainer">
+        <h1> Sign Up</h1>
+      </div>
+    </div>
   )
 }
 export default LoginSignup
