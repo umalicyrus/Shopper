caa2e89c8b76f76b5ac459938c24b142ffe3b422
cyrusumali <umalicyrus04171998@gmail.com>
2024-08-22 15:24:09 +0800

    Add login/signup page and design

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './CSS/LoginSignup.css';
 
 export const LoginSignup = () => {
   return (
     <div className='loginsignup'>
-      <div className="loginsignup-cointainer">
-        <h1> Sign Up</h1>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" id="agree" />
+          <label htmlFor="agree">By continuing, I agree to the terms of use & privacy policy.</label>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-export default LoginSignup
+
+export default LoginSignup;
